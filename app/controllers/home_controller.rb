b3230c05f535e544c8991142e4a3d1a207570ba9
alfredo777 <jose_alfredo@cursa.me>
2014-12-22 13:04:41 -0600

    implementando filtro de logueo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,21 @@
 class HomeController < ApplicationController
+before_filter :session_in
   def index
+
   end
 
   def newsletter
+  end
+private
+  def session_in
+  	if session[:type] != nil
+  		if session[:type] == 'network'
+  			redirect_to institution_path
+  		   else
+  		   if session[:type_user] != nil
+  		      redirect_to "/trainer/#{session[:type_user]}"
+  		   end
+  		end
+  	end
   end
 end
--- app/controllers/register_api_controller.rb
+++ app/controllers/register_api_controller.rb
@@ -1,3 +1,10 @@
 class RegisterApiController < ApplicationController
+   def create_user
+   end
 
+   def create_network
+   end
+
+   def register_session
+   end
 end
