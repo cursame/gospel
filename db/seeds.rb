299036475c17e20fec27d6316b7394f3358c3de4
alfredo777 <jose_alfredo@cursa.me>
2015-01-13 13:20:19 -0600

    pruebas

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,11 @@
 #@network = Network.create(network_name: "prueba pro")
 #@network2 = Network.create(network_name: "prueba")
 ###### creando usuarios de prueba ##########
-#@user1 = User.create(email: 'prueaba1@mail.com', role: 'admin', network_name: 'prueba pro', type_network: 'pro' )
-#@user2 = User.create(email: 'prueaba2@mail.com', role: 'admin', network_name: 'prueba', type_network: 'free' )
-#@user3 = User.create(email: 'prueaba3@mail.com', role: 'teacher', network_name: 'prueba pro', type_network: 'pro' )
-#@user4 = User.create(email: 'prueaba4@mail.com', role: 'teacher', network_name: 'prueba', type_network: 'free' )
-#@user5 = User.create(email: 'prueaba5@mail.com', role: 'student', network_name: 'prueba pro', type_network: 'pro' )
-#@user6 = User.create(email: 'prueaba6@mail.com', role: 'student', network_name: 'prueba', type_network: 'free' )
+@user1 = User.create(email: 'prueba1@mail.com', role: 'admin', network_name: 'prueba pro', type_network: 'pro' )
+@user2 = User.create(email: 'prueba2@mail.com', role: 'admin', network_name: 'prueba', type_network: 'free' )
+@user3 = User.create(email: 'prueba3@mail.com', role: 'teacher', network_name: 'prueba pro', type_network: 'pro' )
+@user4 = User.create(email: 'prueba4@mail.com', role: 'teacher', network_name: 'prueba', type_network: 'free' )
+@user5 = User.create(email: 'prueba5@mail.com', role: 'student', network_name: 'prueba pro', type_network: 'pro' )
+@user6 = User.create(email: 'prueba6@mail.com', role: 'student', network_name: 'prueba', type_network: 'free' )
 ###### creando admin ######
 @admin = UserAdmin.create(email: 'alfredo@cursa.me', password: 'alfredo2008')
